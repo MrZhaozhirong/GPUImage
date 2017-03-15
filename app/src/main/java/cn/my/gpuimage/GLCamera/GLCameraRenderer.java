b7ef03c28ec3c6bc2c2411328a1de3ad195df305
zzr <2377871036@qq.com>
2017-03-15 14:24:08 +0800

    增加一些解读代码的注释

--- app/src/main/java/cn/my/gpuimage/GLCamera/GLCameraRenderer.java
+++ app/src/main/java/cn/my/gpuimage/GLCamera/GLCameraRenderer.java
@@ -42,10 +42,10 @@ public class GLCameraRenderer implements GLSurfaceView.Renderer, Camera.PreviewC
     }
 
     static final float CUBE[] = {
-            -1.0f, -1.0f,
-            1.0f, -1.0f,
-            -1.0f, 1.0f,
-            1.0f, 1.0f,
+            -1.0f, -1.0f,   //左下
+            1.0f, -1.0f,    //右下
+            -1.0f, 1.0f,    //左上
+            1.0f, 1.0f,     //右上
     };
     private final Queue<Runnable> mRunOnDraw;
     private FilterProgram mFilterProgram;
